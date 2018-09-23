f2c89c55f16e58e9970ee14b02a600fc5ce7230e
martin <martin.novak@futuresolutionservices.com>
2018-09-23 12:56:35 +0100

    last update

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -3,7 +3,7 @@
     <v-layout fill-height class="grow0">
       <v-card class="d-inline-block elevation-12">
         <v-navigation-drawer permanent >
-          <v-toolbar flat color="primary" class="pb-3 pt-2">
+          <v-toolbar flat color="#7A589E" class="pb-3 pt-2">
             <v-list>
               <v-list-tile avatar>
                 <v-list-tile-avatar color="white">
@@ -38,9 +38,34 @@
           <v-img src="/images/perseos_logo_horizontal.png" />
         </v-toolbar-title>
       </v-toolbar>
-      <v-layout>
-        <v-flex>Welcome</v-flex>
-      </v-layout>
+      <div class="pa-5">
+        <v-layout row>
+          <v-flex>
+            <v-card class="link-card" to="manageData">
+              <v-card-title class="pink-bg">Manage data</v-card-title>
+              <v-card-text>
+                <p>Competently envisioneer revolutionary platforms whereas focused channels. Interactively exploit resource sucking systems and covalent functionalities.</p>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex class="px-5">
+            <v-card class="link-card" to="activity">
+              <v-card-title class="pink-bg">Activity</v-card-title>
+              <v-card-text>
+                <p>Competently envisioneer revolutionary platforms whereas focused channels. Interactively exploit resource sucking systems and covalent functionalities.</p>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex>
+            <v-card class="link-card" to="services">
+              <v-card-title class="pink-bg">Services</v-card-title>
+              <v-card-text>
+                <p>Competently envisioneer revolutionary platforms whereas focused channels. Interactively exploit resource sucking systems and covalent functionalities.</p>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </div>
     </v-layout>
   </v-layout>
 </template>
@@ -77,10 +102,20 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .grow0 {
-  flex-grow: 0
+  flex-grow: 0;
+  flex-shrink: 0;
 }
 .logo {
   width: 153px;
   height: 60px;
+}
+.pink-bg {
+  background: #7A589E;
+  color: #fff;
+  font-size: 1.3rem;
+  padding-top: 2rem;
+}
+.link-card {
+  cursor: pointer;
 }
 </style>
